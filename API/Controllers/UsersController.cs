77e612d7a5643e92ef751646fde51af8920bf56c
saiteja6399 <saitejadarba@gmail.com>
2024-02-25 20:51:45 +0530

    Initial User functionality

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+[ApiController]
+[Route("/api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _dataContext;
+
+    public UsersController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await _dataContext.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _dataContext.Users.FindAsync(id);
+    }
+}
